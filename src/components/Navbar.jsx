e1625153498f6a9c10ac47f93f9ad063be14e7f7
Ahmad Piracha <68545486+AhmadPiracha@users.noreply.github.com>
2023-07-12 21:03:27 +0500

    maded some changes in navbar and sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
+
+  const toggleMenu = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -45,21 +51,25 @@ const Navbar = () => {
         </ul>
 
         {/* TOGGLE BUTTON AND MENU FOR MOBILE DEVICES */}
-
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div className="sm:hidden flex items-center">
           <img
             src={toggle ? close : menu}
             alt="menu"
             className="w-[20px] h-[28px] object-contain cursor-pointer"
-            onClick={() => setToggle(!toggle)}
+            onClick={toggleMenu}
           />
+        </div>
+      </div>
 
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 `}
-          >
-            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+      {/* Side Menu */}
+      <div
+        className={`fixed top-0 right-0 h-screen w-80 bg-primary bg-opacity-90 z-50 transform transition-transform ${
+          toggle ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="flex justify-end items-center h-full">
+          <div className="w-full bg-primary p-4">
+            <ul className="list-none flex flex-col gap-4">
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
@@ -67,7 +77,7 @@ const Navbar = () => {
                     active === nav.title ? "text-white" : "text-secondary"
                   }`}
                   onClick={() => {
-                    setToggle(!toggle);
+                    toggleMenu();
                     setActive(nav.title);
                   }}
                 >
