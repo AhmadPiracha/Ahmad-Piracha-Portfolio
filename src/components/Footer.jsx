d3fda2520781ae35eae855c5c98ca463084950b2
Ahmad Piracha <AhmadPiracha@users.noreply.github.com>
2023-07-24 17:27:18 +0500

    Footer section Added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-refresh/only-export-components */
+import { SectionWrapper } from "../higherOrderComponents";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="text-white p-4 text-center">
+        <p>
+          Copyright © {new Date().getFullYear()}
+          <span className="text-blue-300"> Ahmad Piracha </span>
+          All rights reserved.
+        </p>
+        <p>
+          Designed by
+          <a
+            href="https://github.com/AhmadPiracha"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-300 hover:text-blue-500"
+          >
+            Ahmad Piracha
+          </a>
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default SectionWrapper(Footer, "");
