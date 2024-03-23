fe6a0cd3fe99f9c6bfd26160e373ebc0cc7da9d2
Ahmad Piracha <ahmadpiracha3@gmail.com>
2024-03-23 19:18:27 +0500

    changed skills section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,53 +1,31 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/no-unknown-property */
-/* eslint-disable react-refresh/only-export-components */
-import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { SectionWrapper } from "../higherOrderComponents";
+import { SectionWrapper } from "../higherOrderComponents"; 
+import { textVariant } from "../utils/motion"; 
 import { technologies } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
 
 import { styles } from "../styles";
-
-const ProgressCard = ({ name, level, isAnimated }) => (
-  <div className="w-[45%] flex justify-center items-center mb-5 lg:mb-0 md:mb-0 sm:mb-10">
-    <motion.div
-      variants={fadeIn("up", "spring", 0.5, 0.75)}
-      className="w-full p-[1px] rounded-[20px]"
-    >
-      <div className="flex justify-between mb-1">
-        <span className="text-base font-medium text-blue-700 dark:text-white">
-          {name}
-        </span>
-        <span className="text-sm font-medium text-blue-700 dark:text-white">
-          {`${level}%`}
-        </span>
-      </div>
-      <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-        <motion.div
-          initial={isAnimated ? { width: 0 } : null}
-          animate={{ width: isAnimated ? `${level}%` : 0 }}
-          transition={{ duration: 2 }} // Adjust the duration value as needed (in seconds)
-          className="bg-blue-700 rounded-full h-2.5 dark:bg-blue-500"
-        ></motion.div>
-      </div>
-    </motion.div>
-  </div>
-);
-
+// eslint-disable-next-line react-refresh/only-export-components
 const Tech = () => {
-  const [isAnimated, setIsAnimated] = useState(false);
-  const { ref, inView } = useInView({
-    triggerOnce: true, // Only trigger the animation once
-    threshold: 0.2, // Adjust this threshold as needed
-  });
+  // Render a category with technologies
+  const renderTechnologies = (category) => {
+    return (
+      <div key={category} className="mb-8">
+      <h3 className="text-xl font-semibold text-gray-900 dark:text-white mb-4 justify-center text-center">
+        {category} 
+      </h3>
+      <div className="flex flex-col gap-2">
+        {technologies[category].map((technology) => (
+          <div key={technology.name} className="flex items-center p-2 bg-white dark:bg-gray-800 rounded-lg shadow-md hover:shadow-lg justify-center text-center"> 
+            <span className="text-base font-medium text-gray-900 dark:text-white ">
+              {technology.name}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+    );
+  };
 
-  useEffect(() => {
-    if (inView) {
-      setIsAnimated(true);
-    }
-  }, [inView]);
 
   return (
     <>
@@ -55,18 +33,22 @@ const Tech = () => {
         <p className={styles.sectionSubText}>My Skills</p>
         <h2 className={styles.sectionHeadText}>Technologies.</h2>
       </motion.div>
-      <div className="mt-10 flex flex-wrap gap-8" ref={ref}>
-        {technologies.map((technology, index) => (
-          <ProgressCard
-            key={technology.name}
-            index={index}
-            isAnimated={isAnimated}
-            {...technology}
-          />
-        ))}
-      </div>
+
+      <div className="container mx-auto px-4 pt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div> 
+            {renderTechnologies("Programming_Languages")} 
+          </div>
+          <div>
+            {renderTechnologies("Frameworks")}
+          </div>
+          <div>
+            {renderTechnologies("Tools")}
+          </div> 
+        </div> 
+      </div> 
     </>
   );
 };
-
+// eslint-disable-next-line react-refresh/only-export-components
 export default SectionWrapper(Tech, "skills");
